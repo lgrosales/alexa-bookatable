74952fd19430351e18592926b01d8f057f2fdd98
Leo <leonardogrosales@gmail.com>
2016-11-26 16:58:50 +0000

    Add AddBookingCommand

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 /**
+ * One-shot model:
+ *  Not Implemented
  * Dialog model:
  *  User: "Alexa, tell Bookatable to book a table at [9PM]"
  *  Alexa: "Of course! For how many people?"
@@ -8,10 +10,13 @@
 
 'use strict';
 
-var APP_ID = "amzn1.ask.skill.e6e3f015-f473-4c82-b3b7-aaa9332d81e5";
-var KEY_TIME = "time"
+var APP_ID = "amzn1.ask.skill.e6e3f015-f473-4c82-b3b7-aaa9332d81e5"; // process.env.APP_ID
+var CREATE_BOOKING_ENDPOINT = "https://{apiHost}/v2/bookings"; // process.env.CREATE_BOOKING_ENDPOINT
+var RESTAURANT_ID = "f115f0f9a5d44318b64c7f84c94dd9fc"; // process.env.RESTAURANT_ID
+var KEY_TIME = "time";
 
 var AlexaSkill = require('./AlexaSkill');
+// var http = require('http');
 
 var BookatableSkill = function () {
     AlexaSkill.call(this, APP_ID);
@@ -46,17 +51,74 @@ BookatableSkill.prototype.intentHandlers = {
         response.ask("Of course! For how many people?", "How many people do you want me to book for?");
     },
     "PartySizeIntent": function (intent, session, response) {
-        handleBooking(intent, session, response);
+        handleCreateBooking(intent, session, response);
     }
 };
 
-function handleBooking(intent, session, response) {
+function handleCreateBooking(intent, session, response) {
     var time = session.attributes[KEY_TIME];
     var partySize = intent.slots.PartySize.value;
     var speechOutput = "A table for " + partySize + " will be ready for you at " + time + " in your favourite restaurant";
     var cardTitle = "Booking confirmed!";
-    console.log("BookatableSkill handleBooking: " + speechOutput);
+    
+    console.log("BookatableSkill handleCreateBooking: " + speechOutput);
+    
+    createBooking(time, partySize);
+    
     response.tellWithCard(speechOutput, cardTitle, speechOutput);
+}
+
+function createBooking(time, partySize) {
+    var addBookingCommand = getAddBookingCommand(time, partySize);
+    console.log("BookatableSkill addBookingCommand: " + JSON.stringify(addBookingCommand));
+
+    //  var post_options = {
+    //   host: CREATE_BOOKING_ENDPOINT,
+    //   port: '80',
+    //   path: '',
+    //   method: 'POST',
+    //   headers: {
+    //       'Content-Type': 'application/json',
+    //       'Content-Length': Buffer.byteLength(addBookingCommand)
+    //     }
+    // };
+
+    // var post_req = http.request(post_options, function(res) {
+    //     res.setEncoding('utf8');
+    //     res.on('data', function (chunk) {
+    //         console.log('Response: ' + chunk);
+    //     });
+    // });
+
+    // post_req.write(addBookingCommand);
+    // post_req.end();
+}
+
+function getAddBookingCommand(time, partySize) {
+    var bookingDateTimeUtc = getBookingDateTimeUtc(time);
+    return {
+        salesforceCustomerId: RESTAURANT_ID,
+        partySize: partySize,
+        dateTimeUtc: bookingDateTimeUtc,
+        channel: "Alexa",
+        bookingSource: "Alexa",
+        walkIn: false,
+        shiftId: null,
+        guest: {
+            firstName: "HAL",
+            lastName: "9000",
+            phoneNumber: "9000",
+            emailAddress: "hal@9000.ai",
+        },
+        notes: "I'm sorry, Dave. I'm afraid I can't do that."
+    };
+}
+
+function getBookingDateTimeUtc(time) {
+    var today = new Date();
+    var month = today.getMonth() + 1;
+    var bookingDateTimeString = today.getFullYear() + "-" + month + "-" + today.getDate() + "T" + time + "+00:00"; //"2016-11-26T19:00+00:00"
+    return new Date(bookingDateTimeString);
 }
 
 exports.handler = function (event, context) {
